8e0690fa57fcd3b9e01f805a5a4c526b567e888a
Juilliard <Juilliardwu0405@gmail.com>
2023-02-13 21:53:33 -0500

    Complete TO DO 1 in Executive class

--- src/main/java/com/mycompany/agency/Executive.java
+++ src/main/java/com/mycompany/agency/Executive.java
@@ -2,7 +2,7 @@ package com.mycompany.agency;
 
 
     // TODO 1: Make Executive a child of StaffEmploee
-public class Executive 
+public class Executive extends StaffEmployee
 {
     private double bonus;
 
