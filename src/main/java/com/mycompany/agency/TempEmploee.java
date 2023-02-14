a57a0dd25c0f93fbd25927611cc7e150c8638867
George <nakagk@farmingdale.edu>
2023-02-13 20:17:52 -0500

    Finished To Do 1, making TempEmploee a child of StaffEmployee

--- src/main/java/com/mycompany/agency/TempEmploee.java
+++ src/main/java/com/mycompany/agency/TempEmploee.java
@@ -4,7 +4,7 @@ package com.mycompany.agency;
 
     // TODO 1: Make TempEmploee a child of StaffEmploee
 
-public class TempEmploee 
+public class TempEmploee extends StaffEmployee
 {
     private int hoursWorked;
 
