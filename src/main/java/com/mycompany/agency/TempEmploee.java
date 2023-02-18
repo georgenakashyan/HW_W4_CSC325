d2f35ee4fda1b1594ad41f44bbcfe937209b9442
George <nakagk@farmingdale.edu>
2023-02-17 23:54:02 -0500

    Todo 4

--- src/main/java/com/mycompany/agency/TempEmploee.java
+++ src/main/java/com/mycompany/agency/TempEmploee.java
@@ -42,6 +42,7 @@ public class TempEmploee extends StaffEmployee
     //-----------------------------------------------------------------
     public String toString()
     {
-
+        return super.toString() + "\nCurrent hours: " + 
+                this.hoursWorked + "\nPaid: " + this.pay();
     }
 }
